f6ad6938f8f255ea766aaa869f257133f83866a9
Marcello Vincent <marcellovincent1502@gmail.com>
2023-09-10 12:47:49 +0200

    update conditions

--- azerty/script.js
+++ azerty/script.js
@@ -1,1 +1,37 @@
 console.log("Hello World");
+
+let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let score = 0
+
+let motUtilisateur = prompt("Entrez le premier mot : " + listeMots[0])
+
+if (motUtilisateur === listeMots[0]) {
+    score += 1
+    console.log("Bravo !")
+} else {
+    console.log("Vous avez fait une erreur de frappe.")
+}
+
+console.log("Voici votre score: " + score)
+
+ motUtilisateur = prompt("Entrez le deuxième mot : " + listeMots[1])
+
+if (motUtilisateur === listeMots[1]) {
+    score ++
+    console.log("Bravo ! ")
+} else {
+    console.log("Vous avez fait une erreur de frappe.")
+}
+
+console.log("Voici votre score: " + score)
+
+motUtilisateur = prompt("Entrez le troisième mot : " + listeMots[2])
+
+if (motUtilisateur === listeMots[2]) {
+    score ++
+    console.log("Bravo !")
+} else {
+    console.log("Vous avez fait une erreur de frappe.")
+}
+
+console.log("Voici votre score: " + score)
