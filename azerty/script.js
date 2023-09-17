726b6a55bb99e7905ef1782d59e37d23c12a72a3
Marcello Vincent <marcellovincent1502@gmail.com>
2023-09-17 18:59:57 +0200

    update

--- azerty/script.js
+++ azerty/script.js
@@ -1,48 +1,52 @@
-// Display a first message
-console.log("Hello World");
-
-// Initialise score and pageActivated
-let score = 0
-let pageActivated = true
-
-// Loop to know tha teven if "mots" or "phrases" is spelled wrong, the question is still asked
-while (pageActivated){
-    let choix = prompt("Entrez le type de liste: mots ou phrases")
-
-// Conditions according to the word written in the prompt
-    switch (choix){
-        case "mots":
-            for ( let i = 0; i<listeMots.length; i++) {
-
-                let motUtilisateur = prompt("Entrez le premier mot : " + listeMots[i])
-
-                if (motUtilisateur === listeMots[i]) {
-                    score += 1
-                    console.log("Bravo !")
-                } else {
-                    console.log("Vous avez fait une erreur de frappe.")
-                }
-
-                console.log("Voici votre score: " + score)
-            }
-            break
-        case "phrases":
-            for ( let i = 0; i<listPhrases.length; i++) {
-
-                let motUtilisateur = prompt("Entrez le premier mot : " + listPhrases[i])
-
-                if (motUtilisateur === listPhrases[i]) {
-                    score += 1
-                    console.log("Bravo !")
-                } else {
-                    console.log("Vous avez fait une erreur de frappe.")
-                }
-
-                console.log("Voici votre score: " + score)
-            }
-            break
-        default: 
-        console.log("Mots inccorecte")
-            break
-    }
+function afficherResultat(score, nbMots)
+{
+    console.log("Vous avez trouvé "+ score+ " sur les "+ nbMots  + " mots.")
 }
+
+function choisirPhrasesOuMots()
+{ 
+    // Set the variable of user's choice
+    let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+    // while not choosen "mots" or "phrases", we ask it again
+    while (choix !== "mots" && choix !== "phrases") 
+        choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+    return choix
+    }
+
+function lancerBoucleDeJeu(liste)
+{
+    let score =0
+    for ( let i = 0; i<liste.length; i++) {
+
+        let motUtilisateur = prompt("Entrez le premier mot : " + liste[i])
+
+        if (motUtilisateur === liste[i]) {
+            score += 1
+            console.log("Bravo !")
+        } else {
+            console.log("Vous avez fait une erreur de frappe.")
+        }
+    }
+    return score
+}
+
+
+function lancerJeu(){
+    
+    let choix =choisirPhrasesOuMots()
+    let nbMots = 0
+    if (choix=== "mots"){
+        lancerBoucleDeJeu(listeMots)
+        nbMots = listeMots.length
+        score += 1
+        afficherResultat(score, nbMots)
+    }else{
+        lancerBoucleDeJeu(listPhrases)
+        nbMots = listPhrases.length
+        afficherResultat(score, nbMots)
+        
+    }
+
+}
+
+lancerJeu()
