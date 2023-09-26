1f6eb597e09b9f65cb4e75f6037654333c88c03e
Marcello Vincent <marcellovincent1502@gmail.com>
2023-09-26 10:32:35 -0400

    update

--- azerty/script/script.js
+++ azerty/script/script.js
@@ -1,50 +1,131 @@
-function afficherResultat(score, nbMots)
-{
-    console.log("Vous avez trouvé "+ score+ " sur les "+ nbMots  + " mots.")
+
+function afficherResultat(score, nbMotsProposes) {
+    // Récupération de la zone dans laquelle on va écrire le score
+    let spanScore = document.querySelector(".zoneScore span")
+    // Ecriture du texte
+    let affichageScore = `${score} / ${nbMotsProposes}` 
+    // On place le texte à l'intérieur du span. 
+    spanScore.innerText = affichageScore
 }
 
-function choisirPhrasesOuMots()
-{ 
-    // Set the variable of user's choice
-    let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
-    // while not choosen "mots" or "phrases", we ask it again
-    while (choix !== "mots" && choix !== "phrases") 
-        choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
-    return choix
-    }
-
-function lancerBoucleDeJeu(liste)
-{
-    let score =0
-    for ( let i = 0; i<liste.length; i++) {
-
-        let motUtilisateur = prompt("Entrez le premier mot : " + liste[i])
-
-        if (motUtilisateur === liste[i]) {
-            score += 1
-            console.log("Bravo !")
-        } else {
-            console.log("Vous avez fait une erreur de frappe.")
-        }
-    }
-    return score
+function afficherProposition(proposition) {
+    let zoneProposition = document.querySelector(".zoneProposition")
+    zoneProposition.innerText = proposition
 }
 
+function afficherEmail(nom, email, score) {
+    let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`
+    location.href = mailto
+}
 
-function lancerJeu(){
+function validerNom(nom) {
+    if (nom.length < 2) {
+        throw new Error("Le nom est trop court. ")
+    }
     
-    let choix = choisirPhrasesOuMots()
-    let nbMots = 0
-    let score = 0
-    if (choix=== "mots"){
-        score = lancerBoucleDeJeu(listeMots)
-        nbMots = listeMots.length
-        afficherResultat(score, nbMots)
-    }else{
-        score = lancerBoucleDeJeu(listPhrases)
-        nbMots = listPhrases.length
-        afficherResultat(score, nbMots)
+}
+
+function validerEmail(email) {
+    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
+    if (!emailRegExp.test(email)) {
+        throw new Error("L'email n'est pas valide.")
+    }
+    
+}
+
+function afficherMessageErreur(message) {
+    
+    let spanErreurMessage = document.getElementById("erreurMessage")
+
+    if (!spanErreurMessage) {
+        let popup = document.querySelector(".popup")
+        spanErreurMessage = document.createElement("span")
+        spanErreurMessage.id = "erreurMessage"
         
+        popup.append(spanErreurMessage)
+    }
+    
+    spanErreurMessage.innerText = message
+}
+
+function gererFormulaire(scoreEmail) {
+    try {
+        let baliseNom = document.getElementById("nom")
+        let nom = baliseNom.value
+        validerNom(nom)
+    
+        let baliseEmail = document.getElementById("email")
+        let email = baliseEmail.value
+        validerEmail(email)
+        afficherMessageErreur("")
+        afficherEmail(nom, email, scoreEmail)
+
+    } catch(erreur) {
+        afficherMessageErreur(erreur.message)
+    }
+    
+}
+
+function lancerJeu() {
+    // Initialisations
+    initAddEventListenerPopup()
+    let score = 0
+    let i = 0
+    let listeProposition = listeMots
+
+    let btnValiderMot = document.getElementById("btnValiderMot")
+    let listeBtnRadio = document.querySelectorAll(".optionSource input")
+    let inputEcriture = document.getElementById("inputEcriture")
+
+
+    afficherProposition(listeProposition[i])
+
+    // Gestion de l'événement click sur le bouton "valider"
+    btnValiderMot.addEventListener("click", () => {
+        if (inputEcriture.value === listeProposition[i]) {
+            score++
+        }
+        i++
+        afficherResultat(score, i)
+        inputEcriture.value = ''
+        if (listeProposition[i] === undefined) {
+            afficherProposition("Le jeu est fini")
+            // On désactive le bouton valider
+            btnValiderMot.disabled = true
+            // On désactive les boutons radios
+            for (let indexBtnRadio = 0; indexBtnRadio < listeBtnRadio.length; indexBtnRadio++) {
+                listeBtnRadio[indexBtnRadio].disabled = true
+            }
+
+        } else {
+            afficherProposition(listeProposition[i])
+        }
+    })
+
+    // Gestion de l'événement change sur les boutons radios. 
+    
+    for (let index = 0; index < listeBtnRadio.length; index++) {
+        listeBtnRadio[index].addEventListener("change", (event) => {
+            // Si c'est le premier élément qui a été modifié, alors nous voulons
+            // jouer avec la listeMots. 
+            if (event.target.value === "1") {
+                listeProposition = listeMots
+            } else {
+                // Sinon nous voulons jouer avec la liste des phrases
+                listeProposition = listePhrases
+            }
+            // Et on modifie l'affichage en direct. 
+            afficherProposition(listeProposition[i])
+        })
     }
 
-}
+    // Gestion de l'événement submit sur le formulaire de partage. 
+    let form = document.querySelector("form")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        let scoreEmail = `${score} / ${i}`
+        gererFormulaire(scoreEmail)
+    })
+
+    afficherResultat(score, i)
+} 
